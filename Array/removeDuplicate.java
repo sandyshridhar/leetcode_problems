2ee95fe86a1a5403905b73f112251392312d17f3
Shridhar <shridharduttaragi4@gmail.com>
2024-01-05 20:18:49 +0530

    modified files

--- Array/removeDuplicate.java
+++ Array/removeDuplicate.java
@@ -7,8 +7,43 @@ import java.util.Set;
 
 public class removeDuplicate {
     public static void main(String args[]){
-        int arr[]={0,0,1,1,1,2,2,3,3,4};
+        int arr[]={1,1,1,2,2,3,3,4};
         findDuplicate( arr );
+        removeDupli(arr);
+        removesorteddupli(arr);
+    }
+
+    private static void removesorteddupli(int[] arr) {
+        int i=0;
+        int j=i+1;
+        int n=arr.length;
+        while (i<n &&j<n){
+            if(arr[i]==arr[j]){
+               j++;
+            }else if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        for (int k=0;k<arr.length;k++){
+            System.out.print(arr[k]+",");
+        }
+        System.out.println();
+    }
+
+    private static void removeDupli(int[] arr) {
+        int tmp[]=new int[arr.length];
+        int j=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]!=arr[i+1]){
+                tmp[j++]=arr[i];
+            }
+        }
+        tmp[j++]=arr[arr.length-1];
+        for(int i=0;i<tmp.length;i++){
+            System.out.print(tmp[i]+",");
+        }
+        System.out.println();
     }
 
     private static void findDuplicate(int[] arr) {
--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int a[]={1,4,1,3,2,5,1};
+        Arrays.sort(a);
+        int i=0;int j=i+1;
+        int n=a.length;
+        while (i<n && j<n){
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            if(a[i]==a[j]){
+                j++;
+            }else if(a[i]!=a[j]){
+                a[i+1]=a[j];
+                i++;
+                //j=i+1;
+            }
+        }
+        for(int k=0;k<a.length;k++){
+            System.out.print(a[k]+",");
         }
     }
 }
